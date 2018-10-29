627ded09e45a8a88227f96b0cc0e965399758079
alumno_fp <jesus.perez@escuelaestech.es>
2018-10-29 18:48:01 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Variable booleana para imprimir el resultado final.
+        boolean capicua=false;
+        int numero;
+        //Pedimos el número y lo asignamos a la variable
+        System.out.println("Introduzca un número entre 0 y 9.999:\n");
+        Scanner sc=new Scanner(System.in);
+        numero=sc.nextInt();
+        //Comprobamos que el número introducido esta en el rango que hemos definido, y si no lo está, volvemos a pedir un número por consola
+        while(numero<0||numero>9999) {
+            System.out.println("Introduzca un número entre 0 y 9.999:\n");
+            numero = sc.nextInt();
+        }
+
+        //Separamos el número en sus componentes.
+        int unidades=numero%10;
+        numero=numero/10;
+        int decenas=numero%10;
+        numero=numero/10;
+        int centenas=numero%10;
+        numero=numero/10;
+        int millares=numero;
+
+
+        //Comprobamos cada componente de mayor grado por si es cero, y en función de eso, comprobamos si las cifras del numero son iguales desde los extremos.
+        if (millares!=0){
+            if((unidades==millares)&&(decenas==centenas)){
+                capicua=true;
+            }
+        }else{
+            if(centenas!=0){
+                if(unidades==centenas){
+                    capicua=true;
+                }
+            }else{
+                if(decenas!=0){
+                    if(unidades==decenas){
+                        capicua=true;
+                    }
+                }else{
+                    System.out.println("El número es de una sola cifra.");
+                }
+            }
+        }
+        if(capicua){
+            System.out.println("El número introducido es capicua.");
+        }else{
+            System.out.println("El número introducido no es capicua.");
+        }
+    }
+}
